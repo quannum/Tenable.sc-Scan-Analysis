f9dddfe7c09d45c9f41c6843f65f27faf821c404
quannum <kyomakino@gmail.com>
2026-05-09 21:30:31 -0400

    Update VERSION
    - Update VERSION in constants.py

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from openpyxl.styles import Font, PatternFill
 
-VERSION = "0.1.0"
+VERSION = "1.8"
 
 
 def default_output_file(run_datetime: datetime | None = None) -> Path:
